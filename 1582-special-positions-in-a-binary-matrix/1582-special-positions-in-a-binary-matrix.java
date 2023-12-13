09d079e867cf084d088ef640a3a65e7b3ab4ebbd
Charu Lata <50863278+charulata1234@users.noreply.github.com>
2023-12-13 12:32:01 +0530

    Time: 4 ms (17.17%), Space: 44.8 MB (8.08%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -3,15 +3,16 @@ class Solution {
         int count=0;
         for(int i=0;i<mat.length;i++){
             for(int j=0;j<mat[0].length;j++){
-                int sum=0;
+                int sumRow=0;
+                int sumCol=0;
                 if(mat[i][j]==1){
                     for(int k=0;k<mat.length;k++){
-                        sum+=mat[k][j];
+                        sumRow+=mat[k][j];
                     }
                     for(int k=0;k<mat[0].length;k++){
-                        sum+=mat[i][k];
+                        sumCol+=mat[i][k];
                     }
-                    if(sum==2){
+                    if(sumCol+sumRow==2){
                         count++;
                     }
 
