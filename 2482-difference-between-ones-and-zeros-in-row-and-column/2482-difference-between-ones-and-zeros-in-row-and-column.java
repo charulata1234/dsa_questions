c520c4df0744b952758b37c588e24e4acf8f1ed1
Charu Lata <50863278+charulata1234@users.noreply.github.com>
2023-12-14 23:06:44 +0530

    Time: 7 ms (79.61%), Space: 80.1 MB (18.12%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.java
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.java
@@ -1,24 +1,25 @@
 class Solution {
     public int[][] onesMinusZeros(int[][] grid) {
-        int[] rowdiff = new int[grid.length];
-        int[] coldiff = new int[grid[0].length];
-
+        int[] rowdiff1 = new int[grid.length];
+        int[] coldiff1 = new int[grid[0].length];
+int[] rowdiff0 = new int[grid.length];
+        int[] coldiff0 = new int[grid[0].length];
         //calculate the diff arrays
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
                 if(grid[i][j] == 1){
-                    rowdiff[i]++;
-                    coldiff[j]++;
+                    rowdiff1[i]++;
+                    coldiff1[j]++;
                 } else {
-                    rowdiff[i]--;
-                    coldiff[j]--;
+                    rowdiff0[i]++;
+                    coldiff0[j]++;
                 }
             }
         }
         
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
-                grid[i][j] = rowdiff[i] + coldiff[j];
+                grid[i][j] = rowdiff1[i] + coldiff1[j]-rowdiff0[i]-coldiff0[j];
             }
         }
 
