7bd2fe93f89bf9d0decee41e0b15d96559798e97
Charu Lata <50863278+charulata1234@users.noreply.github.com>
2023-12-15 09:24:46 +0530

    Time: 1 ms (100.00%), Space: 43.5 MB (11.45%) - LeetHub

--- 1436-destination-city/1436-destination-city.java
+++ 1436-destination-city/1436-destination-city.java
@@ -1,17 +1,17 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-        Map<String, String> map = new HashMap<>();
+        Map<String, String> pathsMap = new HashMap<>();
         
         for (List<String> path : paths) {
-            map.put(path.get(0), path.get(1));
+            pathsMap.put(path.get(0), path.get(1));
         }
         
-        String destination = paths.get(0).get(1);
+        String destinationCity = paths.get(0).get(1);
         
-        while (map.containsKey(destination)) {
-            destination = map.get(destination);
+        while (pathsMap.containsKey(destinationCity)) {
+            destinationCity = pathsMap.get(destinationCity);
         }
         
-        return destination;
+        return destinationCity;
     }
 }
