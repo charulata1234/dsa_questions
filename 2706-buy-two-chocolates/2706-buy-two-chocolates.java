a67681b4d443591e0e6dfc5abeb40513e4c6b4cc
Charu Lata <50863278+charulata1234@users.noreply.github.com>
2023-12-20 11:52:03 +0530

    Time: 3 ms (5.97%), Space: 43.5 MB (39.74%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,25 +1,25 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
         
-            int chocolatePurchased1=0;
+        int chocolatePurchased1=0;
         int chocolatePurchased2=0;
         int sumPrices=0;
-    PriorityQueue<Integer> choclatePrices = new PriorityQueue<Integer>();
+       PriorityQueue<Integer> choclatePrices = new PriorityQueue<Integer>();
         
         for(int i=0;i<prices.length;i++){
             choclatePrices.add(prices[i]);
         }
         
        
-            chocolatePurchased1=choclatePrices.poll();
+        chocolatePurchased1=choclatePrices.poll();
         chocolatePurchased2=choclatePrices.poll();
             sumPrices=chocolatePurchased1+chocolatePurchased2;
             if(sumPrices<=money){
-    money=money-sumPrices;
-    }
+            money=money-sumPrices;
+         }
             
         
-    return money;
+        return money;
         
-    }
-}
+       }
+   }
