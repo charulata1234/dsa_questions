46411452ee395b0feb3437c1e634b6613a95d7b3
Charu Lata <50863278+charulata1234@users.noreply.github.com>
2024-03-11 19:04:57 +0530

    Time: 7 ms (21.20%), Space: 42.6 MB (19.09%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i=0;
+        int j=0;
+        String res="";
+        while(i<word1.length() && j<word2.length()){
+        res=res+word1.charAt(i);
+        res=res+word2.charAt(j);
+            i++;
+            j++;
+        }
+        while(j<word1.length()){
+              res=res+word1.charAt(j);
+            j++;
+        }
+        while(i<word2.length()){
+              res=res+word2.charAt(i);
+            i++;
+        }
+        return res;
+    }
+}
